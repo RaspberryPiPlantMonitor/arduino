e9ae4656336331ff21c0ba163456c67f15c923e4
Daniel <danielmapar@gmail.com>
2021-06-17 06:28:36 -0400

    Adding support for light relay

--- code/code.ino
+++ code/code.ino
@@ -1,4 +1,4 @@
-#define powerPin 4
+#define lightPin 4
 #define humidityPin A0
 
 float humidityValue = 0;
@@ -7,30 +7,28 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // open the serial port at 9600 bps:
   pinMode(humidityPin, INPUT);
-  pinMode(powerPin, OUTPUT);
+  pinMode(lightPin, OUTPUT);
 
-  digitalWrite(powerPin, LOW); 
+  digitalWrite(lightPin, LOW); 
 }
 
 void loop() {
-  // Check data comming from Raspberry Pi USB port
-  String data = Serial.readStringUntil('\n');
-  if (data.length() == 0) { return; }
-  
-  humidityValue = analogRead(humidityPin);
-  Serial.print("{humidityValue:"+String(humidityValue)+",powerValue:"+data[0]+","+"pumpValue:"+data[1]+"}\n");
+  String data = Serial.readString(); 
 
+  humidityValue = analogRead(humidityPin);
+  Serial.print("{\"humidityValue\":"+String(humidityValue)+",\"lightValue\":"+data[0]+","+"\"pumpValue\":"+data[1]+"}\n");
+  
   // Control IoT Relay
   if (data[0] == '1'){
-    digitalWrite(powerPin, HIGH);  
+    digitalWrite(lightPin, HIGH);  
   } else {
-    digitalWrite(powerPin, LOW);
+    digitalWrite(lightPin, LOW);
   }
-
+  
   // Control Pump - TODO
   if (data[1] == '1'){
     //digitalWrite(powerPin, HIGH);  
   } else {
     //digitalWrite(powerPin, LOW);
-  }
+  }  
 }
